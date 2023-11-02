8f7efc9dcd3156e6b824aa1ce4550ea02b37beb6
Murali Mohan <Muralisocial123@gmail.com>
2023-11-02 20:22:31 +0530

    Chnages are added

--- Index.js
+++ Index.js
@@ -18,5 +18,15 @@ document.getElementById("password").disabled = true;
 document.getElementById("submit").disabled = true;
 return false;
 }
+else{
+    attempt --;// Decrementing by one.
+    alert("You have left "+attempt+" attempt;");
+    // Disabling fields after 3 attempts.
+    if( attempt == 0){
+    document.getElementById("username").disabled = true;
+    document.getElementById("password").disabled = true;
+    document.getElementById("submit").disabled = true;
+    return false;
+    }
 }
 }
